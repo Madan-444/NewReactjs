53ae8c89851f587a8b0a05cf369bfc708f1e81f8
Madan-444 <mmadan3600@gmail.com>
2021-03-24 21:43:20 +0545

    nothing

--- learn-react/src/Components/Tasks.js
+++ learn-react/src/Components/Tasks.js
@@ -6,7 +6,7 @@ function Tasks({tasks}) {
         <div>
             {
                 tasks.map((item)=> <div>
-                    <span> <h1 style={{display:'inline', color:'blue'}}>{item.name}</h1> <h2 style={{display:'inline'}}> {item.text}  </h2> <i className="fa fa-times" ></i> </span> 
+                    <span> <h1 style={{display:'inline', color:'black'}}>{item.name}</h1> <h2 style={{display:'inline'}}> {item.text}  </h2> <i className="fa fa-times" ></i> </span> 
                 </div>)
             }
         </div>
