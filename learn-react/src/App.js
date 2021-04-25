d80eb89f70eefdf314eea1f5fd85dfe8e52ddd2b
Madan-444 <mmadan3600@gmail.com>
2021-04-25 19:47:13 +0545

    animation learned

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,31 +1,23 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import "./App.css";
-import FormHandling from "./Components/FormHandling";
-import ComponentC from "./Components/Hooks/ComponentC";
-import HooksWithObject from "./Components/HooksWithObject";
-import UseEffectInReact from "./Components/UseEffectInReact";
-import UseOfLifeCycle from "./Components/UseOfLifeCycle";
-import Header from "./Header";
-// import ComponentC from './Components/Hooks/ComponentC'
-
-// create context
-
-const MyContext = React.createContext();
 
 function App() {
-  // const [tasks,setTasks] = useState(initialState)
-  return (
-    <div className="App">
-      {/* <FormHandling /> */}
-      {/* <UseOfLifeCycle /> */}
-      {/* <UseEffectInReact /> */}
-      {/* <HooksWithObject /> */}
-      {/* <Header /> */}
-      <MyContext.Provider value = "Madan">
-        <ComponentC />
-      </MyContext.Provider>
+  return <div className='pa-container'>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ducimus omnis, in iure placeat reprehenderit sit consequuntur ratione praesentium veniam ea odit quisquam aliquid nobis necessitatibus laboriosam error cum consequatur?
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis quis alias quos iste nam unde dolorem reiciendis officia consectetur nihil saepe, eos, nostrum illo recusandae accusamus, esse in exercitationem. Aut?
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus odit cupiditate, eveniet vitae vel doloremque minima dicta. Labore reiciendis debitis dignissimos doloribus? Qui, quia voluptates? Blanditiis reprehenderit error veritatis earum.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum animi quibusdam cupiditate assumenda officia vel ullam, iure nobis veniam? Aspernatur totam vel quas ullam, nam adipisci porro accusamus itaque eaque.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ad nulla eos odio voluptatibus cum eaque ut maiores enim, eligendi quibusdam voluptatem mollitia excepturi pariatur minus perspiciatis maxime nesciunt ipsum!
+
+
+
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quod, ipsum qui rerum quaerat ea? Molestias aspernatur nobis maiores, eum cum quasi perspiciatis, corrupti, dolorem accusantium nesciunt nemo? Inventore, non.
+    <div className="box-container">
+    <div className="box">
+      <p>Container</p>
     </div>
-  );
+    </div>
+  </div>
 }
 
 export default App;
