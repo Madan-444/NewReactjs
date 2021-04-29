5f432741a864efbc8a5f57f547ee2138f8ebb68f
Madan-444 <mmadan3600@gmail.com>
2021-04-29 09:20:10 +0545

    cursor effect using react

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -2,22 +2,44 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  let myRef = React.useRef(null)
+  let cursorRef = React.useRef(null)
+  React.useEffect(()=> {
+    let myLinks = myRef.current.childNodes
+    let myCursorClass = cursorRef.current
+    console.log("mycursorClass",myCursorClass)
+
+    window.addEventListener("mousemove",cursor)
+
+    function cursor(e) {
+      // console.log('The e',e)
+      myCursorClass.style.top = e.pageY + 'px';
+      myCursorClass.style.left = e.pageX + 'px';
+
+    }
+    myLinks.forEach((link)=> {
+      link.addEventListener('mouseleave',()=> {
+        myCursorClass.classList.remove('link-grow')
+        link.classList.remove('hovered-link')
+    })
+    link.addEventListener('mouseover',()=> {
+      myCursorClass.classList.add('link-grow')
+        link.classList.add('hovered-link')
+    })
+
+    })
+  },[])
   return <div className='pa-container'>
-    <div className="cursor"></div>
+    <div className="cursor" ref = {cursorRef}></div>
     <nav>
       <h1>Ma Dan</h1>
-      <ul className='nav-links'>
+      <ul className='nav-links' ref={myRef}>
         <li>Home</li>
         <li>Projects</li>
         <li>My Vlogs</li>
         <li>Services</li>
       </ul>
     </nav>
-    <div className="box-container">
-    <div className="box">
-      <p>Container</p>
-    </div>
-    </div>
   </div>
 }
 
